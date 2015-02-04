b28d69b86224d229a33ac1fb1a9ce4f4aae2bfdb
Jim Berry <jim@jimberry.net>
2015-02-04 15:00:02 -0700

    {Test Suite} Dunkin Local and Dunkin Dev configurations

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -124,14 +124,17 @@ testConfigurations{
         oauth {
             url {
                 tacobell = "https://dev01-app.cardfree.net/"
+                dunkin = "https://dev-dun-api01.cardfree.net/"
                 checkers = "https://dev-mon-api01.cardfree.net/"
             }
             id {
                 tacobell = "e28833aa-64df-4541-bd16-67d80d55b558"
+                dunkin = "B95D6A3EA972448BBB15C23DD0D20C25"
                 checkers = "D93B5A92DA0D11E3BF4B005056B046D7"
             }
             secret {
                 tacobell = "03d91f77-973e-4d11-a3cb-ef860e12994e"
+                dunkin = "670166E094F34196928A87E893B39FC1"
                 checkers = "D9556C1DDA0D11E3BF4B005056B046D7"
             }
         }
@@ -149,12 +152,15 @@ testConfigurations{
         api {
             address {
                 tacobell = "http://192.168.82.128/"
+                dunkin = "http://192.168.82.128/"
             }
             account_management_application {
                 tacobell = "vsvm/"
+                dunkin = "vsvm/"
             }
             order_management_application {
                 tacobell = "vsvm_order/"
+                dunkin = "vsvm_order/"
             }
         }
     }
@@ -162,26 +168,32 @@ testConfigurations{
         oauth {
             url {
                 tacobell = "https://dev01-app.cardfree.net/"
+                dunkin = "https://dev-dun-api01.cardfree.net/"
                 checkers = "https://dev-mon-api01.cardfree.net/"
             }
             id {
                 tacobell = "e28833aa-64df-4541-bd16-67d80d55b558"
+                dunkin = "B95D6A3EA972448BBB15C23DD0D20C25"
                 checkers = "D93B5A92DA0D11E3BF4B005056B046D7"
             }
             secret {
                 tacobell = "03d91f77-973e-4d11-a3cb-ef860e12994e"
+                dunkin = "670166E094F34196928A87E893B39FC1"
                 checkers = "D9556C1DDA0D11E3BF4B005056B046D7"
             }
         }
         api {
             address {
                 tacobell = "https://dev01-app.cardfree.net/"
+                dunkin = "https://dev-dun-api01.cardfree.net/"
             }
             account_management_application {
                 tacobell = "account-management/v1/"
+                dunkin = "account-management/v1/"
             }
             order_management_application {
                 tacobell = "order-management/v1/"
+                dunkin = "order-management/v1/"
             }
         }
         svc {
