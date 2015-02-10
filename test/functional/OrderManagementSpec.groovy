03440a3eb818712a6cf87d736fa82b9580928fac
Jim Berry <jim@jimberry.net>
2015-02-10 09:48:28 -0700

    {Test Suite} Better autoreload test

--- test/functional/OrderManagementSpec.groovy
+++ test/functional/OrderManagementSpec.groovy
@@ -46,8 +46,76 @@ class OrderManagementSpec extends FunctionalSpecBase{
         then:
         savedGCCheckoutData != null
         OrderManagementService.validateSubmitOrderResponse(submitOrderResult)
+
+        //Update Card Balance
+        when:
+        def getBalanceResult = giftCardService.getGiftCardBalance(userToken, addGCResult?.json?.cardId)
+
+        then:
+        getBalanceResult != null
+
+        //Check Transaction History
+        when:
+        def transactionHistoryResult = giftCardService.getGiftCardTransactionHistory(userToken, addGCResult?.json?.cardId)
+
+        then:
+        transactionHistoryResult != null
     }
 
+    def "Submit order with saved Gift Card to trigger auto reload"(){
+        //Create New User
+        when:
+        def userResult = accountManagementService.provisionNewRandomUser()
+
+        then:
+        AccountManagementService.validateNewUser(userResult)
+
+        //Login User
+        when:
+        def userToken = accountManagementService.getRegisteredUserToken(userResult.json.email, config.userInformation.password)
+
+        then:
+        !userToken.isEmpty()
+
+        //Add GC to user using a new Visa CC
+        when:
+        def addGCResult = giftCardService.provisionGiftCardWithNewCC(10.00, false, false, userToken, CreditCardService.CreditCardType.VISA)
+
+        then:
+        GiftCardService.validateNewGiftCardResult(addGCResult)
+
+        //Create Order
+        when:
+        def createOrderResult = orderManagementService.createOrder(userToken)
+
+        then:
+        OrderManagementService.validateCreateOrderResponse(createOrderResult)
+
+        //Submit Order
+        when:
+        def savedGCCheckoutData = orderManagementService.getSavedGiftCardCheckoutData(addGCResult?.json?.cardId)
+        def submitOrderResult = orderManagementService.submitOrder(userToken, createOrderResult?.json?.orderId, savedGCCheckoutData)
+
+        then:
+        savedGCCheckoutData != null
+        OrderManagementService.validateSubmitOrderResponse(submitOrderResult)
+
+        //Update Card Balance
+        when:
+        def getBalanceResult = giftCardService.getGiftCardBalance(userToken, addGCResult?.json?.cardId)
+
+        then:
+        getBalanceResult != null
+
+        //Check Transaction History
+        when:
+        def transactionHistoryResult = giftCardService.getGiftCardTransactionHistory(userToken, addGCResult?.json?.cardId)
+
+        then:
+        transactionHistoryResult != null
+    }
+
+    @Ignore
     def "Submit GUEST order with new CC"(){
         //Setup Device Identifier
         setup:
@@ -71,7 +139,6 @@ class OrderManagementSpec extends FunctionalSpecBase{
         OrderManagementService.validateSubmitOrderResponse(submitOrderResult)
     }
 
-    @Ignore
     def "Submit order with new Credit Card"(){
         //Create New User
         when:
@@ -102,6 +169,14 @@ class OrderManagementSpec extends FunctionalSpecBase{
         then:
         creditCardCheckoutDetails != null
         OrderManagementService.validateSubmitOrderResponse(submitOrderResult)
+
+        //Check Transaction History
+        when:
+        def transactionHistoryResult = giftCardService.getGiftCardTransactionHistory(userToken, "b5033382-73b5-46bc-a39c-af2b969a75c4")
+
+        then:
+        transactionHistoryResult != null
+
     }
 
 }
